b0ac0b9cbae7bb7c77ba3222d7261275278a3c92
c0b23009 <c0b23009bd@edu.teu.ac.jp>
2024-09-24 16:05:11 +0900

    練習問題7-1,7-2　ワープ問題を解消

--- pygame_image.py
+++ pygame_image.py
@@ -12,13 +12,17 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg") #1
     koka_img = pg.image.load("fig/3.png") #2
     koka_img = pg.transform.flip(koka_img,True,False) #2
+    bg1_img = pg.transform.flip(bg_img,True,False)#7-1
     tmr = 0 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = -(tmr % 800) #6
+        x = -(tmr % 3200) #6 #7-1 #7-2
         screen.blit(bg_img, [x, 0]) #3 #6
+        screen.blit(bg1_img,[x+1600,0]) #7-1
+        screen.blit(bg_img, [x+3200, 0]) #7-2
+        screen.blit(bg_img, [x+4800, 0]) #7-2
         screen.blit(koka_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
