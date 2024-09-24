cb4e70ae35dce5925708d28a415c3180a038d333
c0b23009 <c0b23009bd@edu.teu.ac.jp>
2024-09-24 15:29:26 +0900

    練習問題4 こうかとんを表示

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #3
+        screen.blit(koka_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
